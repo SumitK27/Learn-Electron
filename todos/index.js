fe9469a61ecc56a39c13ec1c59602dd34f919cd2
SumitK27 <sumitkajbaje@gmail.com>
2022-01-29 17:06:02 +0530

    Initial Setup

---
+++ todos/index.js
@@ -0,0 +1,14 @@
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+app.on("ready", () => {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadFile("main.html");
+});
