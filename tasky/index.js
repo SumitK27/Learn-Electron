0335a8c4b5c79e1d3ce1b5e399ff9f27071aaff0
SumitK27 <sumitkajbaje@gmail.com>
2022-01-30 18:48:41 +0530

    Added Blur Event

--- tasky/index.js
+++ tasky/index.js
@@ -20,6 +20,11 @@ app.on("ready", () => {
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
+    // Hide window if user clicks somewhere else
+    mainWindow.on("blur", () => {
+        mainWindow.hide();
+    });
+
     const iconName =
         process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
