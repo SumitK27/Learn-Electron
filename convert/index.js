bd4cd7100eda29949d01082a9ff1d09b406dc4f1
SumitK27 <sumitkajbaje@gmail.com>
2022-01-31 19:53:23 +0530

    Get Videos and return the metadata

--- convert/index.js
+++ convert/index.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const ffmpeg = require("fluent-ffmpeg");
+const _ = require("lodash");
 
 let mainWindow;
 
@@ -13,4 +15,22 @@ app.on("ready", () => {
     });
 
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on("videos:added", (event, videos) => {
+    const promises = _.map(videos, (video) => {
+        // Create a promise that will resolve with the video data
+        return new Promise((resolve, reject) => {
+            ffmpeg.ffprobe(video.path, (err, metadata) => {
+                video.duration = metadata.format.duration;
+                video.format = "avi";
+                resolve(video);
+            });
+        });
+    });
+
+    // Wait for all videos to be processed
+    Promise.all(promises).then((results) => {
+        mainWindow.webContents.send("metadata:complete", results);
+    });
 });
